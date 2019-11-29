2b92f06a8785dd53d59bd1f189039e56834a5385
20161204010052 <20161204010052@LAJ291417.ifrn.local>
2019-11-29 10:44:24 -0200

    Application

---
+++ literaturese/src/main/java/com/literaturese/LiteratureseApplication.java
@@ -0,0 +1,13 @@
+package com.literaturese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteratureseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteratureseApplication.class, args);
+	}
+
+}
